fba78deaa67a5ab3ed2ea2e47b1c1a02622cbbf4
BeryStudio <58179846+RaulBerecki@users.noreply.github.com>
2025-05-16 20:52:41 +0300

    last update

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movingSpeed;
+    public float movingSpeed,timeStamp;
     Vector2 movement;
     Rigidbody2D rb;
+    public int stamina;
+    bool isRunning;
     // Start is called before the first frame update
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        stamina=100;
+        timeStamp = 0;
     }
 
     // Update is called once per frame
@@ -18,6 +22,32 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetKey(KeyCode.LeftShift) && stamina >0)
+        {
+            timeStamp += Time.deltaTime;
+            movingSpeed =3;
+            if (timeStamp >= 0.3f)
+            {
+                stamina--;
+                timeStamp = 0;
+            }
+        }
+        else
+        {
+            movingSpeed =2;
+        }
+        if(!Input.GetKey(KeyCode.LeftShift))
+        {
+            if (stamina < 100)
+            {
+                timeStamp += Time.deltaTime;
+                if (timeStamp >= 0.5f)
+                {
+                    stamina++;
+                    timeStamp = 0;
+                }
+            }
+        }
     }
     private void FixedUpdate()
     {
